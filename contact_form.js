d1355e74ee1848f06e6c70593b89c6cc7015774c
remus <remusandreinicula@gmail.com>
2026-02-25 14:24:58 +0200

    Lab 2:adaugare ce am facut sapt trecuta

---
+++ contact_form.js
@@ -0,0 +1,9 @@
+ 
+
+function submitForm() {
+  const nume = document.getElementById("name").value; 
+  const nume2 = document.getElementById("email").value; 
+  const nume3 = document.getElementById("story").value; 
+  console.log(nume);
+  console.warn("Goodbye World!");
+} 0
