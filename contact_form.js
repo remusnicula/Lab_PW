09c5f18d30293300b44ac1ce22d8b0107c8961ed
remus <remusandreinicula@gmail.com>
2026-03-04 15:00:09 +0200

    ex 3 from lab 2

--- contact_form.js
+++ contact_form.js
@@ -1,9 +1,42 @@
  
+function sss(){
+  const now = new Date();
+  const h = now.getHours();
+  const my_text = document.getElementById("p1");
+  if(h=>6 && 12<h){
+    my_text.textContent = "BBBBB";
+  }
+  if(h>=12 && h<17)
+  {
+    my_text.textContent = "CCCCCCCCC";
+  }
+  if(h>17 && h<6)
+  {
+    my_text.textContent = "DDDDDDDDDD";
+  }
+}
 
 function submitForm() {
   const nume = document.getElementById("name").value; 
-  const nume2 = document.getElementById("email").value; 
-  const nume3 = document.getElementById("story").value; 
+
+  const email = document.getElementById("email").value; 
+  const mesaj = document.getElementById("story").value; 
   console.log(nume);
-  console.warn("Goodbye World!");
-} 0
+  console.log(email.includes("@"));
+  const my_text1 = document.getElementById("p2");
+
+  if((email.includes("@")) && (nume.length > 2) && (mesaj.length > 10)) {
+    document.getElementById("p2").innerHTML = ":)";
+    console.log("wadawdas");
+  }
+  else{
+    document.getElementById("p2").innerHTML = "WAAAAAAAAAAAA";
+  }
+} 
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+sss();
+ 
