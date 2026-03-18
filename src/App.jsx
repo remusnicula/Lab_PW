95128cfa8d5146ede9ac05d6cfa63188dfe27f8f
remus <remusandreinicula@gmail.com>
2026-03-18 15:12:11 +0200

    card+ex4

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Card from './Card';
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const projects = [
+ { title: "Proiect 1", description: "Pagina personala" },
+ { title: "Proiect 2", description: "Calculator buget" },
+ { title: "Proiect 3", description: "Dashboard React" },
+ { title: "Proiect 4", description: "you spelt it wrong  " },
+];
+ return (
+ <div>
+ <h1>Dashboard</h1>
+ 
+{projects.map(function(item, index) {
+ return <Card key={index} title={item.title} description={item.description} />;
+})}
+ </div>
+ );
 }
-
-export default App
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+ 
+function Card(props) {
+ return (
+ <div>
+ <h3>{props.title}</h3>
+ <p>{props.description}</p>
+ </div>
+ 
+ );
+ 
+}
+export default Card;
+ 
